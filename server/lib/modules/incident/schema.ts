331c14c91d3825c9b60f09317ad28ce13f610f44
juanalb <juan@zimmermanzimmerman.com>
2020-10-21 13:12:58 +0200

    feat: improved schema validation with enums

--- server/lib/modules/incident/schema.ts
+++ server/lib/modules/incident/schema.ts
@@ -1,18 +1,18 @@
-import mongoose, {Schema} from 'mongoose';
+import mongoose, { Schema } from "mongoose";
 
 const IncidentSchema = new mongoose.Schema({
-    reportDate: Date,
-    subject: String,
-    type: String,
-    reportedBy: { type: Schema.Types.ObjectId, ref: 'User' },
-    priority: String,
-    deadline: Number,
-    description: String,
-    isResolved: Boolean,
+  reportDate: Date,
+  subject: String,
+  type:  { type: String, enum: ["Software", "Hardware", "Service"], default: "Software"},
+  reportedBy: { type: Schema.Types.ObjectId, ref: "User" },
+  priority: { type: String, enum: ["Low", "Normal", "High"], default: "Low"},
+  deadline: Date,
+  description: String,
+  isResolved: Boolean
 });
 
-IncidentSchema.set('toJSON', {
-    virtuals: true
+IncidentSchema.set("toJSON", {
+  virtuals: true
 });
 
-export default mongoose.model('Incidents', IncidentSchema);
+export default mongoose.model("Incidents", IncidentSchema);
--- server/lib/modules/user/schema.ts
+++ server/lib/modules/user/schema.ts
@@ -1,13 +1,17 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    role: String,
+    role: { type: String, enum: ["Service Desk employee", "Regular employee"], default: "Regular employee"},
     firstName: String,
     lastName: String,
     phoneNumber: String,
     email: String,
     password: String,
-    location: String,
+    location: { type: String, enum: ["Amsterdam", "Haarlem", "Knuppeldam", "HQ"], default: "Amsterdam"},
+});
+
+userSchema.set('toJSON', {
+    virtuals: true
 });
 
 export default mongoose.model('users', userSchema);
