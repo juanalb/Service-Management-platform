17e8e2892b35d787a4fbce29a1d5c1aaee64c528
juanalb <26707584+juanalb@users.noreply.github.com>
2023-01-23 18:10:11 +0100

    implemented incident search by email

--- client/src/components/tabpanels/IncidentManagement.tsx
+++ client/src/components/tabpanels/IncidentManagement.tsx
@@ -1,7 +1,7 @@
 import React, {useContext, useState} from "react";
 import {
-  Container,
-  Button,
+    Container,
+    Button, TextField,
 } from "@material-ui/core";
 import DataTable from "../datatable/DataTable";
 import { incidentColumns } from "../datatable/DataTable-constants";
@@ -9,28 +9,61 @@ import AddIcon from "@material-ui/icons/Add";
 import { Link } from "react-router-dom";
 import {incidentAPI} from "../../api/IncidentAPI";
 import {AuthContext} from "../../context/AuthContext";
+import {IUser} from "../../../../server/lib/modules/user/model";
+import {IIncident, IncidentResponse} from "../../../../server/lib/modules/incident/model";
 
 
 export default function IncidentManagement() {
   const [incidents, setIncidents] = useState([]);
+  const [shownIncidents, setShownIncidents] = useState([]);
+  const [searchPrompt, setSearchPrompt] = useState<string>();
   const { role, userId } = useContext(AuthContext);
 
-    React.useEffect(() => {
+
+  React.useEffect(() => {
       if(role === 'Service Desk Employee') {
           incidentAPI.getAllIncidents().then(res => {
               setIncidents(res.data.data)
+              setShownIncidents(res.data.data)
           }).catch(e => {
               console.log(e)
           })
-      } else if(role === 'Regular Employee'){
+      } else if(role === 'Regular Employee') {
           incidentAPI.getAllIncidentsByUserId(userId).then(res => {
               setIncidents(res.data.data)
+              setShownIncidents(res.data.data)
           }).catch(e => {
               console.log(e)
           })
       }
-
   }, []);
+
+    React.useEffect(() => {
+        if(searchPrompt === ''){
+            setShownIncidents(incidents);
+            return;
+        }
+
+        const filteredIncidents = filterByEmail();
+        setShownIncidents(filteredIncidents);
+    }, [searchPrompt]);
+
+    function filterByEmail() {
+        if(!searchPrompt){
+            return [];
+        }
+
+        return incidents.filter((incident: IncidentResponse) => {
+            const searchString = searchPrompt.toLowerCase().replace(/\s+/g, ''); // removes all whitespace
+            const email = incident.reportedBy.email.toLowerCase();
+
+            return email.includes(searchString);
+        })
+    }
+
+    const handleSearchTextFieldChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchPrompt(event.target.value);
+    };
 
   return (
     <Container maxWidth="lg">
@@ -39,9 +72,12 @@ export default function IncidentManagement() {
           Create Incident
         </Button>
       </Link>
+        { role === 'Service Desk Employee' &&
+            <TextField id="user-search" label="Search user by name or mail" onChange={handleSearchTextFieldChange} />
+        }
       <DataTable
         columns={incidentColumns}
-        data={incidents}
+        data={shownIncidents}
         loading={incidents.length <= 0}
       />
     </Container>
--- server/lib/modules/incident/model.ts
+++ server/lib/modules/incident/model.ts
@@ -1,11 +1,24 @@
 import mongoose from "mongoose";
+import {IUser} from "../user/model";
 
 export interface IIncident {
   _id?: String;
   reportDate: Date;
   subject: String;
   type: "Software" | "Hardware" | "Service";
-  reportedBy: mongoose.Schema.Types.ObjectId;
+  reportedBy: mongoose.Schema.Types.ObjectId | IUser;
+  priority: "Low" | "Normal" | "High";
+  deadline: Date;
+  description: String;
+  isResolved: Boolean;
+}
+
+export interface IncidentResponse {
+  _id?: String;
+  reportDate: Date;
+  subject: String;
+  type: "Software" | "Hardware" | "Service";
+  reportedBy: IUser;
   priority: "Low" | "Normal" | "High";
   deadline: Date;
   description: String;
